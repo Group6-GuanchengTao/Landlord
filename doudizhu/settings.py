4ff02a50fb91e4007b10f6286f6157d6886faccf
S9Championship <55094266+S9Championship@users.noreply.github.com>
2021-09-22 15:06:36 +0800

    Add files via upload

---
+++ doudizhu/settings.py
@@ -0,0 +1,62 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+TEMPLATE_ROOT = os.path.join(BASE_DIR, 'templates')
+
+DEBUG = os.getenv('TORNADO_DEBUG') == 'True'
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'fiDSpuZ7QFe8fm0XP9Jb7ZIPNsOegkHYtgKSd4I83Hs=')
+
+PORT = os.getenv('PORT', 8080)
+
+WECHAT_CONFIG = {
+    'appid': os.getenv('APPID'),
+    'appsecret': os.getenv('APPSECRET'),
+    'token': os.getenv('TOKEN'),
+    'encoding_aes_key': os.getenv('ENCODING_AES_KEY'),
+}
+
+
+def database_url(url):
+    from urllib.parse import urlparse, ParseResult
+    pr: ParseResult = urlparse(url)
+    return {
+        'host' : pr.hostname,
+        'database': pr.path[1:],
+        'user': pr.username,
+        'password': pr.password,
+    }
+
+
+DATABASE = database_url(os.getenv('DATABASE_URL', 'mysql://root:root@127.0.0.1:3306/ddz'))
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'root': {
+        'level': 'INFO',
+        'handlers': ['console'],
+        'propagate': True,
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s %(levelname)s %(module)s %(lineno)d %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'tornado.general': {
+            'handlers': ['console'],
+            'propagate': True,
+        },
+    }
+}
